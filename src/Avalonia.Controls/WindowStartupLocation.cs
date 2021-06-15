c8fe723706505295ab9f63190e18df68cca4f847
Nathan Garside <code@nathangarside.com>
2021-06-15 16:10:38 +0100

    Update WindowStartupLocation.cs

--- src/Avalonia.Controls/WindowStartupLocation.cs
+++ src/Avalonia.Controls/WindowStartupLocation.cs
@@ -18,26 +18,6 @@
         /// <summary>
         /// The startup location is the center of the owner window. If the owner window is not specified, the startup location will be <see cref="Manual"/>.
         /// </summary>
-        CenterOwner,
-
-        /// <summary>
-        /// The startup location is the upper left corner of the screen.
-        /// </summary>
-        UpperLeftScreen,
-
-        /// <summary>
-        /// The startup location is the upper right corner of the screen.
-        /// </summary>
-        UpperRightScreen,
-
-        /// <summary>
-        /// The startup location is the lower right corner of the screen.
-        /// </summary>
-        LowerRightScreen,
-
-        /// <summary>
-        /// The startup location is the lower left corner of the screen.
-        /// </summary>
-        LowerLeftScreen,
+        CenterOwner
     }
 }
